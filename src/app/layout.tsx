abf3f68a66372b3ace3d4a45bc4c95bcf167351a
yash <yashgouravkar@gmail.com>
2025-10-24 11:26:32 +0530

    ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { MainLayout } from '~/components/layouts/main';
+import { Providers } from '~/components/layouts/providers';
 import { siteConfig } from '~/lib/site';
 import './globals.css';
 
@@ -47,7 +49,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          <MainLayout>{children}</MainLayout>
+        </Providers>
       </body>
     </html>
   );
