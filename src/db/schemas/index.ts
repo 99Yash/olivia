b7d2284f1bbae597e0c3cdd3afc5c514ef9d822c
yash <yashgouravkar@gmail.com>
2025-10-25 12:46:28 +0530

    feat: add drizzle migrations

--- src/db/schemas/index.ts
+++ src/db/schemas/index.ts
@@ -1,1 +1,5 @@
 export * from './auth';
+export * from './job';
+export { jobRelations } from './job';
+export * from './resume';
+export { resumeRelations } from './resume';
---
+++ src/db/schemas/job.ts
@@ -0,0 +1,25 @@
+import { relations } from 'drizzle-orm';
+import { pgEnum, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { lifecycle_dates } from './helpers';
+import { resume } from './resume';
+
+export const jobStatusEnum = pgEnum('job_status', [
+  'pending',
+  'valid',
+  'invalid',
+  'error',
+]);
+
+export const job = pgTable('job', {
+  id: text('id').primaryKey(),
+  url: varchar('url').notNull().unique(),
+  status: jobStatusEnum('status').default('pending').notNull(),
+  content: text('content'),
+  analyzedAt: timestamp('analyzed_at'),
+  invalidReason: text('invalid_reason'),
+  ...lifecycle_dates,
+});
+
+export const jobRelations = relations(job, ({ many }) => ({
+  resumes: many(resume),
+}));
